d6418ba9ab0f841df376b5531788cf1da4b5e643
Stephen Clibbery <sdclibbery@googlemail.com>
2017-12-03 00:41:39 +0000

    day 2 2

--- day-2.hs
+++ day-2.hs
@@ -3,9 +3,13 @@ main = do
         input <- readFile "day-2-input.txt"
         print $ process input
 
-process = show . sum . map range . map parse . map words . lines
+process = show . sum . map exactQuotient . map parse . map words . lines
 
 parse :: [String] -> [Int]
 parse = map read
 
-range xs = (maximum xs) - (minimum xs)
+exactQuotient :: [Int] -> Int
+exactQuotient = head . map fst . filter ((== 0) . snd) . map (uncurry quotRem) . filter (uncurry (/=)) . combinations
+
+combinations :: [Int] -> [(Int, Int)]
+combinations xs = [ (x,y) | x<-xs, y<-xs ]
