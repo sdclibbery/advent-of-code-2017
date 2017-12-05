f13a13634b5703ad3e3646944e31941065b2b965
Stephen Clibbery <sdclibbery@googlemail.com>
2017-12-05 00:07:22 +0000

    day 3 tidy

--- day-3.hs
+++ day-3.hs
@@ -7,11 +7,12 @@ main = print
     $ manhattanDistance
     $ coords input
   where
-    manhattanDistance (x,y) = (abs x) + (abs y)
-    counts = concatMap (replicate 2) [1..]
-    countedActions = zip counts $ cycle [R,U,L,D]
-    actions = concatMap (\x -> replicate (fst x) (snd x)) countedActions
     coords n = foldr move (0,0) $ take (n-1) actions
+      where
+        counts = concatMap (replicate 2) [1..] -- 1,1,2,2,3,3,4,4...
+        countedActions = zip counts $ cycle [R,U,L,D] -- r1,u1,l2,d2,r3,u3,l4...
+        actions = concatMap (\x -> replicate (fst x) (snd x)) countedActions -- rullddrrruuullllddddrrrrr...
+    manhattanDistance (x,y) = (abs x) + (abs y)
 
 move R (x,y) = (x+1, y)
 move U (x,y) = (x, y+1)
