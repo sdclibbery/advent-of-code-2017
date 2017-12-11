06614c05a4547c251ec3a6c7006ccd34254106b2
Stephen Clibbery <sdclibbery@googlemail.com>
2017-12-11 00:09:16 +0000

    day 4 part 2

--- day-4.rs
+++ day-4.rs
@@ -1,16 +1,27 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn is_valid (pass_phrase: &str) -> bool {
     let mut used_words = HashSet::new();
     for word in pass_phrase.split_whitespace() {
-        if used_words.contains(word) {
+        let sorted_word = sort_word(&word);
+        if used_words.contains(&sorted_word) {
             return false;
         }
-        used_words.insert(word);
+        used_words.insert(sorted_word);
     }
     return true;
+}
+
+fn sort_word (word: &str) -> String {
+    let s_slice: &str = &word[..];
+
+    let mut chars: Vec<char> = s_slice.chars().collect();
+    chars.sort_by(|a, b| b.cmp(a));
+
+    return String::from_iter(chars);
 }
 
 fn main () {
