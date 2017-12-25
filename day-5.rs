9d32ea2827e84dd4d867ca4db7182d82a3997e3f
Stephen Clibbery <sdclibbery@googlemail.com>
2017-12-25 23:57:21 +0000

    day 5 part 1 in rust

--- day-5.rs
+++ day-5.rs
@@ -7,5 +7,19 @@ fn main () {
     let mut input = String::new();
     file.read_to_string(&mut input).expect("something went wrong reading the file");
 
-    println!("{:?}", input.lines().count());
+//    println!("{:?}", run( &mut [0, 3, 0, 1, -3] ));
+    println!("{:?}", run(&mut input.lines().map(|x| x.parse().unwrap()).collect::<Vec<i32>>()) );
+}
+
+fn run (jumps: &mut [i32]) -> i32 {
+    let mut pc: i32 = 0;
+    let mut num_steps: i32 = 0;
+    let num_jumps: i32 = (jumps.iter().count()) as i32;
+    while pc >=0 && pc < num_jumps {
+        let jump_index: usize = pc as usize;
+        pc = pc + jumps[jump_index];
+        jumps[jump_index] += 1;
+        num_steps += 1;
+    }
+    return num_steps;
 }
